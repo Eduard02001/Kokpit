4cc8a6ff791d1fb1c4d54ccfca9122dbaf0f11e0
Eduardo Ribeiro <eduardo.ribeiro@britania.com.br>
2026-08-01 12:51:29 -0300

    chore: escafolda backend NestJS com TypeORM (SQL Server)
    Configura ConfigModule (.env), TypeOrmModule apontando para SQL Server
    (synchronize desabilitado, migrations versionadas), ValidationPipe
    global, exception filter e CORS com credenciais para a futura
    autenticação por sessão. Nenhuma entity/rota de negócio ainda.

---
+++ backend/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import typeormConfig from './config/typeorm.config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeormConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        configService.get('typeorm')!,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
